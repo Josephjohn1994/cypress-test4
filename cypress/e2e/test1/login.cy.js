8e7d7c7704a40963c65d7e0918d737cc43ac4c3c
unknown <joph941@gmail.com>
2024-12-12 13:08:06 +0530

    cypress 1

---
+++ cypress/e2e/test1/login.cy.js
@@ -0,0 +1,9 @@
+describe('User Registration Flow', () => {
+    it('should allow a user to sign up with valid details', () => {
+      cy.visit('https://www.meesho.com/');
+     
+    });
+  
+    
+  });
+  
